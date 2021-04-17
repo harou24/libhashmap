3f845a7013da40226eb8d017ee204730ee7f0c3c
Harou <haroutioun24@gmail.com>
2021-04-17 02:03:56 +0200

    hm_get_seq updated to the last version

--- src/hm_get_seq.c
+++ src/hm_get_seq.c
@@ -12,13 +12,25 @@
 
 #include "hashmap_internal.h"
 
+static	void	set_pair_to_null(t_pair *pair)
+{
+	pair->u_f.key = NULL;
+	pair->u_s.value = NULL;
+}
+
 t_pair	hm_get_seq(const void *_hm)
 {
 	const t_hash_map	*hm = (t_hash_map *)_hm;
 	static t_hm_node	*node;
 	t_pair				pair;
 
-	if (node)
+	if (_hm == NULL)
+	{
+		node = NULL;
+		set_pair_to_null(&pair);
+		return (pair);
+	}
+	if (node != NULL)
 		node = node->next;
 	else
 		node = hm->first_node;
@@ -30,8 +42,7 @@ t_pair	hm_get_seq(const void *_hm)
 	else
 	{
 		node = hm->first_node;
-		pair.u_f.key = NULL;
-		pair.u_s.value = NULL;
+		set_pair_to_null(&pair);
 	}
 	return (pair);
 }
